640c286124cb551d9b9095ed60f2558a6923a65e
Mark Olbert <mark@arcabama.com>
2023-05-25 11:44:34 -0700

    convert to class

---
+++ RouteSnapper/SourceFileInfo.cs
@@ -0,0 +1,28 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace RouteSnapper;
+
+public class SourceFileInfo : ObservableObject
+{
+    private bool _isSelected;
+    private string? _path;
+
+    public string? FileName => System.IO.Path.GetFileName( Path );
+
+    public bool IsSelected
+    {
+        get => _isSelected;
+        set => SetProperty( ref _isSelected, value );
+    }
+
+    public string? Path
+    {
+        get => _path;
+
+        set
+        {
+            SetProperty( ref _path, value );
+            OnPropertyChanged( nameof( FileName ) );
+        }
+    }
+}
