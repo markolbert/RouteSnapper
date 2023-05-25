854b9f89cdfc4b152be4c82e3161e3fc1b55f3be
Mark Olbert <mark@arcabama.com>
2023-05-25 11:42:26 -0700

    add view models for nav pages

---
+++ RouteSnapper/viewmodels/ExportViewModel.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Windows.Storage;
+using Windows.Storage.Pickers;
+using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+
+namespace RouteSnapper;
+
+public class ExportViewModel : ObservableObject
+{
+    private string? _exportPath;
+    private StorageFile? _exportFile;
+
+    public ExportViewModel()
+    {
+        SelectExportFileCommandAsync = new AsyncRelayCommand( SelectExportFileHandlerAsync );
+    }
+
+    public string? ExportPath
+    {
+        get => _exportPath;
+        set => SetProperty( ref _exportPath, value );
+    }
+
+    public List<string> ExportTypes { get; } = new();
+
+    public AsyncRelayCommand SelectExportFileCommandAsync { get; }
+
+    private async Task SelectExportFileHandlerAsync()
+    {
+        var picker = new FileSavePicker
+        {
+            SuggestedStartLocation = PickerLocationId.Desktop, 
+            SuggestedFileName = "Snapped Route",
+
+        };
+
+        var hWnd = WinRT.Interop.WindowNative.GetWindowHandle(App.Current.MainWindow);
+        WinRT.Interop.InitializeWithWindow.Initialize(picker, hWnd);
+
+        picker.SuggestedStartLocation = PickerLocationId.Desktop;
+        picker.FileTypeChoices.Add("Gpx", new List<string>(){".gpx"}   );
+        picker.FileTypeChoices.Add("Kml", new List<string>(){".kml"});
+        picker.FileTypeChoices.Add("Kmz", new List<string>() { ".kmz" });
+
+        _exportFile = await picker.PickSaveFileAsync();
+        ExportPath = _exportFile?.Path;
+    }
+}
---
+++ RouteSnapper/viewmodels/SourceFilesViewModel.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Threading.Tasks;
+using Windows.Storage.Pickers;
+using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+
+namespace RouteSnapper;
+
+public class SourceFilesViewModel : ObservableObject
+{
+    private int _selectedSrcIdx;
+    private bool _enableSelectClearSrc;
+
+    public SourceFilesViewModel()
+    {
+        AddSourceFileCommand = new AsyncRelayCommand( AddSourceFileHandlerAsync );
+        RemoveSourceFileCommand = new RelayCommand<int>( RemoveSourceFileHandler );
+        ClearSelectedSourceFileCommand = new RelayCommand( ClearSelectedSourceFileHandler );
+        SelectAllSourceFilesCommand = new RelayCommand( SelectAllSourceFilesHandler );
+        ClearAllSourceFilesCommand = new RelayCommand( ClearAllSourceFilesHandler );
+    }
+
+    public ObservableCollection<SourceFileInfo> SourceFiles { get; } = new();
+
+    public AsyncRelayCommand AddSourceFileCommand { get; }
+
+    private async Task AddSourceFileHandlerAsync()
+    {
+        var picker = new FileOpenPicker
+        {
+            ViewMode = PickerViewMode.List, 
+            SuggestedStartLocation = PickerLocationId.Desktop
+        };
+
+        var hWnd = WinRT.Interop.WindowNative.GetWindowHandle(App.Current.MainWindow);
+        WinRT.Interop.InitializeWithWindow.Initialize(picker, hWnd);
+
+        picker.FileTypeFilter.Add(".gpx");
+        picker.FileTypeFilter.Add(".kml");
+        picker.FileTypeFilter.Add(".kmz");
+
+        var files = await picker.PickMultipleFilesAsync();
+
+        foreach( var file in files )
+        {
+            if( SourceFiles.Any( x => x.Path?.Equals( file.Path, StringComparison.OrdinalIgnoreCase ) ?? false ) )
+                continue;
+
+            SourceFiles.Add( new SourceFileInfo { IsSelected = true, Path = file.Path } );
+        }
+
+        EnableSelectClearSourceFiles = SourceFiles.Any();
+    }
+
+    public bool EnableSelectClearSourceFiles
+    {
+        get => _enableSelectClearSrc;
+        set => SetProperty( ref _enableSelectClearSrc, value );
+    }
+
+    public RelayCommand<int> RemoveSourceFileCommand { get; }
+
+    private void RemoveSourceFileHandler( int index )
+    {
+        if( index < 0 || index >= SourceFiles.Count ) 
+            return;
+
+        SourceFiles.RemoveAt( index );
+        EnableSelectClearSourceFiles = SourceFiles.Any();
+    }
+
+    public int SelectedSourceFileIndex
+    {
+        get => _selectedSrcIdx;
+        set => SetProperty( ref _selectedSrcIdx, value );
+    }
+
+    public RelayCommand ClearSelectedSourceFileCommand { get; }
+
+    private void ClearSelectedSourceFileHandler() => SelectedSourceFileIndex = -1;
+
+    public RelayCommand SelectAllSourceFilesCommand { get; }
+
+    private void SelectAllSourceFilesHandler()
+    {
+        foreach( var file in SourceFiles )
+        {
+            file.IsSelected = true;
+        }
+
+        OnPropertyChanged(nameof(SourceFiles));
+    }
+
+    public RelayCommand ClearAllSourceFilesCommand { get; }
+
+    private void ClearAllSourceFilesHandler()
+    {
+        foreach (var file in SourceFiles)
+        {
+            file.IsSelected = false;
+        }
+
+        OnPropertyChanged(nameof(SourceFiles));
+    }
+
+}
