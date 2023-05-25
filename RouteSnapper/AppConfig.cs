74d0aa3e1ef986c972d5698053053bbc1289ef21
Mark Olbert <mark@arcabama.com>
2023-05-25 11:43:40 -0700

    expand configuration

--- RouteSnapper/AppConfig.cs
+++ RouteSnapper/AppConfig.cs
@@ -4,10 +4,25 @@ namespace RouteSnapper;
 
 internal class AppConfig : AppConfigBase
 {
+    public string ProjectionName { get; set; } = string.Empty;
+
     [EncryptedProperty]
     public string BingKey { get; set; } = string.Empty;
 
     [EncryptedProperty]
     public string GoogleKey { get; set; } = string.Empty;
+
+    [EncryptedProperty]
+    public string GoogleSignatureSecret { get; set; } = string.Empty;
+
+    [EncryptedProperty]
+    public string OpenStreetMapsKey { get; set; } = string.Empty;
+
+    [EncryptedProperty]
+    public string OpenTopoMapsKey { get; set; } = string.Empty;
+
+    public string Center { get; set; } = "0,0";
+    public double Heading { get; set; }
+    public double Scale { get; set; }
 }
 
