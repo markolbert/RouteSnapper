483a3fb1a13f0a1c42c5822ccfd9b776d88893aa
Mark Olbert <mark@arcabama.com>
2023-05-26 10:39:09 -0700

    clarify name

--- RouteSnapperLib/constants/GeoConstants.filtering.cs
+++ RouteSnapperLib/constants/GeoConstants.filtering.cs
@@ -25,7 +25,7 @@ public partial class GeoConstants
 {
     public const double DefaultMaxPointSeparationKm = 2.5;
     public const double DefaultMaxRouteGapMeters = 500;
-    public const double DefaultMaxClusterDiameterMeters = 500;
+    public const double DefaultMaxClusterRadiusMeters = 500;
     public const double DefaultMinimumPointGapMeters = 200;
     public const double DefaultMaximumOverallGapMeters = DefaultMinimumPointGapMeters * 5;
     public const double DefaultBearingToleranceDegrees = 15;
