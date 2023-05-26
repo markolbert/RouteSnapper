c530c6613f8be7240940e0eed61e1fc2bd1923a7
Mark Olbert <mark@arcabama.com>
2023-05-26 10:38:17 -0700

    add app constants

---
+++ RouteSnapper/Constants.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Xml;
+using J4JSoftware.RouteSnapper;
+
+namespace RouteSnapper;
+
+public static class Constants
+{
+    public const string GoogleSnapper = "Google";
+    public const string BingSnapper = "Bing";
+
+    public static List<UnitType> MeasurementUnits { get; } = Enum.GetValues<UnitType>().ToList();
+}
