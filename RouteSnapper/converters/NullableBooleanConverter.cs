c9d5b53e2cac7ecbe470da35b27de344aee92d17
Mark Olbert <mark@arcabama.com>
2023-05-26 10:35:39 -0700

    add converter

---
+++ RouteSnapper/converters/NullableBooleanConverter.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using Microsoft.UI.Xaml.Data;
+
+namespace RouteSnapper;
+
+public class NullableBooleanConverter : IValueConverter
+{
+    public object Convert( object? value, Type targetType, object parameter, string language ) => value is bool and true;
+
+    public object ConvertBack( object value, Type targetType, object parameter, string language ) => throw new NotImplementedException();
+}
