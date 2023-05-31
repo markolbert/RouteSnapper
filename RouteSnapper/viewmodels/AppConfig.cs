eebf4137efad20878d4d2717c1aef0bd5320adf0
Mark Olbert <mark@arcabama.com>
2023-05-31 13:14:25 -0700

    add license headers, cleanup, conform to revised API

--- RouteSnapper/viewmodels/AppConfig.cs
+++ RouteSnapper/viewmodels/AppConfig.cs
@@ -1,8 +1,29 @@
-﻿using J4JSoftware.WindowsUtilities;
+﻿#region copyright
+// Copyright (c) 2021, 2022, 2023 Mark A. Olbert 
+// https://www.JumpForJoySoftware.com
+// AppConfig.cs
+//
+// This file is part of JumpForJoy Software's RouteSnapper.
+// 
+// RouteSnapper is free software: you can redistribute it and/or modify it 
+// under the terms of the GNU General Public License as published by the 
+// Free Software Foundation, either version 3 of the License, or 
+// (at your option) any later version.
+// 
+// RouteSnapper is distributed in the hope that it will be useful, but 
+// WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY 
+// or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License 
+// for more details.
+// 
+// You should have received a copy of the GNU General Public License along 
+// with RouteSnapper. If not, see <https://www.gnu.org/licenses/>.
+#endregion
+
+using J4JSoftware.WindowsUtilities;
 
 namespace RouteSnapper;
 
-internal class AppConfig : AppConfigBase
+internal class AppConfig : WinUIConfigBase
 {
     public EngineViewModel EngineViewModel { get; set; } = new();
     public MapViewModel MapViewModel { get; set; } = new();
